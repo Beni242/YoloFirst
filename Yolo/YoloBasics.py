f6625317ff2fa45e0acce2a7ded55d428560a4e7
MBD <divinemountso@gmail.com>
2026-05-08 19:23:49 +0300

    yolo weight separe

--- Yolo/YoloBasics.py
+++ Yolo/YoloBasics.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('yolov8n.pt')
+model = YOLO('../Yolo-Weight/yolov8n.pt') # wait of the img
 
 results = model("images/car.png", show=True)
 cv2.waitKey(0) # unless user input dont do anything
