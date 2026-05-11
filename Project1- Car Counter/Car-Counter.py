00d841c74282e481016dd9772df1e265545196a4
MBD <divinemountso@gmail.com>
2026-05-11 23:55:46 +0300

    trackers

--- Project1- Car Counter/Car-Counter.py
+++ Project1- Car Counter/Car-Counter.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ultralytics import YOLO
 import cv2
 import cvzone # display all detection
@@ -27,6 +28,10 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
 
 mask = cv2.imread("mask.png")
 
+#Tracking
+tracker = Sort(max_age=20, min_hits=5, iou_threshold=0.3)
+
+
 while True:
     sucess, img = cap.read()
 
@@ -36,6 +41,7 @@ while True:
     imgRegion = cv2.bitwise_and(img, mask)
     results = model(imgRegion, stream=True) # only on the specify region it's detect it
 
+    detections = np.empty((0,5))
 
     # design the boxes
     for r in results:
@@ -64,9 +70,24 @@ while True:
              if currurentClass == "car" or currurentClass == "truck" or currurentClass == "bus"\
                      or currurentClass == "motorbike" and conf >= 0.3:
 
-                cvzone.putTextRect(img, f'{classNames[cls]}{conf}', (max(0, x1), max(35, y1)),
-                                   scale=0.7, thickness=1, offset=3)  # display a box text up the zone
-                cvzone.cornerRect(img, (x1, y1, w, h), l=9)
+               # cvzone.putTextRect(img, f'{classNames[cls]}{conf}', (max(0, x1), max(35, y1)),
+                                   #scale=0.7, thickness=1, offset=3)  # display a box text up the zone
+                cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt= 5)
+                currurentArray = np.array([x1,y1,x2,y2,conf]) #list
+                detections = np.vstack((detections, currurentArray))
+
+
+    resultsTracker = tracker.update(detections)
+
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+        print(result)
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w , h), l=1, rt=1, colorR=(255,0,0)) # tracker blocks
+        cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(5, y1)),
+                           scale=2, thickness=3, offset=8)
 
     cv2.imshow('img',img)
     cv2.imshow('imgRegion', imgRegion)
