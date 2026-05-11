24671de20819d10607f7ec140d4f99d0184bb3a7
MBD <divinemountso@gmail.com>
2026-05-12 00:58:35 +0300

    limits

--- Project1- Car Counter/Car-Counter.py
+++ Project1- Car Counter/Car-Counter.py
@@ -31,6 +31,12 @@ mask = cv2.imread("mask.png")
 #Tracking
 tracker = Sort(max_age=20, min_hits=5, iou_threshold=0.3)
 
+#line
+
+limits = [0, 660, 940, 660]  # ligne horizontale pleine largeur, y ≈ 660
+
+
+
 
 while True:
     sucess, img = cap.read()
@@ -47,37 +53,22 @@ while True:
     for r in results:
         boxes = r.boxes
         for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
 
-            # Bounding Box
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
 
-             x1, y1, x2, y2 = box.xyxy[0]
-
-             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-            # with cvzone
-             w, h = x2-x1, y2-y1
-             cvzone.cornerRect(img, (x1, y1, w,h), l=9)
-
-            # confidance
-
-             conf = math.ceil((box.conf[0]*100))/100
-
-            # Class Name
-
-             cls = int (box.cls[0])
-             currurentClass = classNames[cls]
-
-             if currurentClass == "car" or currurentClass == "truck" or currurentClass == "bus"\
-                     or currurentClass == "motorbike" and conf >= 0.3:
-
-               # cvzone.putTextRect(img, f'{classNames[cls]}{conf}', (max(0, x1), max(35, y1)),
-                                   #scale=0.7, thickness=1, offset=3)  # display a box text up the zone
-                cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt= 5)
-                currurentArray = np.array([x1,y1,x2,y2,conf]) #list
-                detections = np.vstack((detections, currurentArray))
+            if currentClass in ["car", "truck", "bus", "motorbike"] and conf >= 0.3:
+                cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
 
 
     resultsTracker = tracker.update(detections)
+    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0,0,255), 5)
 
     for result in resultsTracker:
         x1, y1, x2, y2, id = result
@@ -90,7 +81,7 @@ while True:
                            scale=2, thickness=3, offset=8)
 
     cv2.imshow('img',img)
-    cv2.imshow('imgRegion', imgRegion)
+    #cv2.imshow('imgRegion', imgRegion)
 
     if cv2.waitKey(0) & 0xFF == ord('q'):
         break
